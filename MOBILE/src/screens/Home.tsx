0823aafd94c70641d66ca91fb3710dd0f924b41b
Pedro Teodoro <pedropauloteodoro26@gmail.com>
2023-02-05 01:38:26 -0300

    Aula final concluida

--- MOBILE/src/screens/Home.tsx
+++ MOBILE/src/screens/Home.tsx
@@ -1,20 +1,19 @@
-import { View, Text, ScrollView, Alert } from "react-native";
+import { useCallback, useState } from 'react';
+import { Text, View, ScrollView, Alert } from 'react-native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 
-import dayjs from "dayjs";
-import { api } from "../lib/axios";
+import { api } from '../lib/axios';
 import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
 
-import { useState, useEffect } from "react";
+import { Header } from '../components/Header';
+import { Loading } from '../components/Loading';
+import { HabitDay, DAY_SIZE } from '../components/HabitDay';
+import dayjs from 'dayjs';
 
-import { HabitDay, DAY_SIZE } from "../components/HabitDay";
-import { Header } from "../components/Header";
-import { Loading } from "../components/Loading";
-import { useNavigation } from '@react-navigation/native';
-
-const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const datesFromYearStart = generateRangeDatesFromYearStart();
-const minimumSummaryDatesSizes = 18 * 5;
-const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
+const minimunSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length
 
 type SummaryProps = Array<{
   id: string;
@@ -24,16 +23,16 @@ type SummaryProps = Array<{
 }>
 
 export function Home() {
-  const [loading, setLoading] = useState(true);
-  const [summary, setSummary] = useState<SummaryProps | null >(null);
+  const [loading, setLoading] = useState(true)
+  const [summary, setSummary] = useState<SummaryProps | null>(null)
 
-  const { navigate } = useNavigation();
+  const { navigate } = useNavigation()
 
-  async function fetchData(){
+  async function fetchData() {
     try {
-      setLoading(true);
-      // Verificar se a api esta escutando a porta e se configurou por padrão o local host para que o emulador android receba as info do servidor o modelo é esse aqui abaixo  
-      /*app.listen({
+      setLoading(true)
+       // Verificar se a api esta escutando a porta e se configurou por padrão o local host para que o emulador android receba as info do servidor o modelo é esse aqui abaixo.
+        /*app.listen({
         port: 3333,
         host: '0.0.0.0'
       }).then( () => {
@@ -41,51 +40,50 @@ export function Home() {
       })*/
       const response = await api.get('/summary');
       /*console.log(response.data);*/
-      setSummary(response.data);
+      setSummary(response.data)
     } catch (error) {
-      Alert.alert('Ops','Não foi possível carregar o sumário de hábitos. ');
-      console.log(error);
+      Alert.alert('Ops', 'Não foi possível carregar o sumário de hábitos.')
+      console.log(error)
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
   }
-   
-  useEffect(() => {
-    fetchData();
-  },[]);
 
-  if(loading) {
+  useFocusEffect(useCallback(() => {
+      fetchData()
+    }, []))
+
+  if (loading) {
     return (
       <Loading />
-    );
+    )
   }
 
   return (
-    <View className="flex-1 bg-background px-8 pt-16">
+    <View className='flex-1 bg-background px-8 pt-16'>
       <Header />
 
-        <View className="flex-row mt-6 mb-2">
-          {
-            weekDays.map((weekDay, i) => (
-              <Text
-                key={`${weekDay}-${i}`}
-                className="text-zinc-400 text-xl font-bold text-center mx-1"
-                style={{ width: DAY_SIZE }}
-              >
-                {weekDay}
-              </Text>
-            ))
-          }
+      <View className="flex-row mt-6 mb-2">
+        {
+          weekDays.map((weekDay, i) => (
+            <Text 
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl font-bold text-center mx-1"
+              style={{ width: DAY_SIZE }}
+            >
+              {weekDay}
+            </Text>
+          ))
+        }
+      </View>
 
-        </View>
-
-        <ScrollView
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{ paddingBottom: 100 }}
-        >
-          {
-            summary &&
-            <View className="flex-row flex-wrap">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        {
+          summary && (
+            <View className='flex-row flex-wrap'>
               {
                 datesFromYearStart.map(date => {
                   const dayWithHabits = summary.find(day => {
@@ -93,7 +91,7 @@ export function Home() {
                   })
 
                   return (
-                    <HabitDay
+                    <HabitDay 
                       key={date.toISOString()}
                       date={date}
                       amountOfHabits={dayWithHabits?.amount}
@@ -108,16 +106,18 @@ export function Home() {
                 amountOfDaysToFill > 0 && Array
                 .from({ length: amountOfDaysToFill })
                 .map((_, index) => (
-                  <View
+                  <View 
                     key={index}
                     className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
-                    style={{ width: DAY_SIZE, height: DAY_SIZE}}
+                    style={{ width: DAY_SIZE, height: DAY_SIZE }}
                   />
                 ))
               }
             </View>
-          }
-        </ScrollView>
+          )
+        }
+      </ScrollView>
+
     </View>
   )
-}
+} 
--- MOBILE/src/screens/New.tsx
+++ MOBILE/src/screens/New.tsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
-import { ScrollView, View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
+import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Feather } from '@expo/vector-icons'
+import colors from "tailwindcss/colors";
+
 import { BackButton } from "../components/BackButton";
 import { CheckBox } from "../components/CheckBox";
-import { Feather } from "@expo/vector-icons";
-import colors from "tailwindcss/colors";
 import { api } from "../lib/axios";
 
-const avaliableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
 export function New() {
-  const [title, setTitle] = useState('');
   const [weekDays, setWeekDays] = useState<number[]>([]);
+  const [title, setTitle] = useState('');
 
   function handleToggleWeekDay(weekDayIndex: number) {
-    if(weekDays.includes(weekDayIndex)){
-      setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex));
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex))
     } else {
       setWeekDays(prevState => [...prevState, weekDayIndex])
     }
@@ -22,29 +23,28 @@ export function New() {
 
   async function handleCreateNewHabit() {
     try {
-      if(!title.trim() || weekDays.length === 0 ){
-        Alert.alert('Novo Hábito', 'Informe o nome do hábito e escolha a periodicidade.')
+      if (!title.trim() || weekDays.length === 0) {
+        Alert.alert('Novo hábito', 'Informe o nome do hábito e escolha a periodicidade.')
       }
 
-      await api.post('/habits', { title, weekDays });
-
+      await api.post('/habits', { title, weekDays })
+      
       setTitle('');
       setWeekDays([]);
 
-      Alert.alert('Novo hábito', 'Hábito criado com sucesso!!!')
+      Alert.alert('Novo hábito', 'Hábito criado com sucesso!');
     } catch (error) {
-      console.log(error);
-      Alert.alert('Ops', 'Não foi possível criar o novo hábito');
+      console.log(error)
+      Alert.alert('Ops', 'Não foi possível criar o novo hábito')
     }
   }
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <ScrollView
-       showsHorizontalScrollIndicator={false}
-       contentContainerStyle={{ paddingBottom: 100 }}
+      <ScrollView 
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
       >
-
         <BackButton />
 
         <Text className="mt-6 text-white font-extrabold text-3xl">
@@ -55,9 +55,9 @@ export function New() {
           Qual seu comprometimento?
         </Text>
 
-        <TextInput
-          className="h-12  pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
-          placeholder="Execícios, dormir bem, etc..."
+        <TextInput 
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
+          placeholder="Exercícios, dormir bem, etc..."
           placeholderTextColor={colors.zinc[400]}
           onChangeText={setTitle}
           value={title}
@@ -68,7 +68,7 @@ export function New() {
         </Text>
 
         {
-          avaliableWeekDays.map((weekDay, index) => (
+          availableWeekDays.map((weekDay, index) => (
             <CheckBox
               key={weekDay}
               title={weekDay}
@@ -79,14 +79,14 @@ export function New() {
         }
 
         <TouchableOpacity
-         className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
-         activeOpacity={0.7}
-         onPress={handleCreateNewHabit}
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
         >
-          <Feather
-            name="check"
-            size={20}
-            color={colors.white}
+          <Feather 
+           name="check"
+           size={20}
+           color={colors.white}
           />
 
           <Text className="font-semibold text-base text-white ml-2">
@@ -95,5 +95,5 @@ export function New() {
         </TouchableOpacity>
       </ScrollView>
     </View>
-  );
+  )
 }
